80d58dceb99a7c2da792b4bb274f212733456969
Palmerg4 <71123289+Palmerg4@users.noreply.github.com>
2024-11-06 10:49:32 -0500

    SE-1198 Added tests for 768 add/sub/mul functions; Added 1024 add/sub; Added fuzz tests for 1024 add/sub functions

--- src/Uint1024.sol
+++ src/Uint1024.sol
@@ -27,7 +27,10 @@ library Uint1024 {
             r1 := add(a1, b1)
             let carryoverB := lt(r1, b1)
             r1 := add(r1, carryoverA)
+<<<<<<< HEAD
             carryoverB := or(carryoverB, lt(r1, carryoverA))
+=======
+>>>>>>> e50bd15 (SE-1198 Added tests for 768 add/sub/mul functions; Added 1024 add/sub; Added fuzz tests for 1024 add/sub functions)
             r2 := add(a2, b2)
             carryoverA := lt(r2, b2)
             r2 := add(r2, carryoverB)
@@ -56,6 +59,15 @@ library Uint1024 {
      */
     function sub768x768(uint a0, uint a1, uint a2, uint b0, uint b1, uint b2) internal pure returns (uint r0, uint r1, uint r2) {
         if (lt768(a0, a1, a2, b0, b1, b2)) revert("Uint768: negative result sub768x768");
+<<<<<<< HEAD
+=======
+
+        /*assembly {
+            r0 := sub(a0, b0)
+            r1 := sub(sub(a1, b1), lt(a0, b0))
+            r2 := sub(sub(a2, b2), lt(a1, b1))
+        }*/
+>>>>>>> e50bd15 (SE-1198 Added tests for 768 add/sub/mul functions; Added 1024 add/sub; Added fuzz tests for 1024 add/sub functions)
         
         assembly {
             if or(lt(b0, a0), eq(b0, a0)) {
@@ -202,7 +214,6 @@ library Uint1024 {
         }
         // slither-disable-end divide-before-multiply
     }
-
     function mulInverseMod512(uint b0, uint b1) internal pure returns (uint inv0, uint inv1) {
         if (b0 % 2 == 0) revert("Uint1024: denominator must be odd");
         (uint bx3Lo, uint bx3Hi) = b0.mul512x256(b1, 3);
@@ -382,5 +393,132 @@ library Uint1024 {
             }
             r3 := sub(a3, b3)
         }
+    }
+
+    /**
+     * @notice Checks the minuend(a0-a3) is greater than the right operand(b0-b3)
+     * @dev Used as an underflow/negative result indicator for subtraction methods
+     * @param a0 A uint256 representing the lower bits of the minuend
+     * @param a1 A uint256 representing the high bits of the minuend
+     * @param a2 A uint256 representing the higher bits of the minuend
+     * @param a3 A uint256 representing the highest bits of the minuend
+     * @param b0 A uint256 representing the lower bits of the subtrahend
+     * @param b1 A uint256 representing the high bits of the subtrahend
+     * @param b2 A uint256 representing the higher bits of the subtrahend
+     * @param b3 A uint256 representing the highest bits of the subtrahend
+     * @return Returns true if there would be an underflow/negative result
+     */
+    function lt1024(uint a0, uint a1, uint a2, uint256 a3, uint b0, uint b1, uint b2, uint256 b3) internal pure returns (bool) {
+        return a3 < b3 || (a3 == b3 && a2 < b2) || (a2 == b2 && a1 == b1 && a0 < b0);
+    }
+
+    /**
+     * @notice Calculates the sum of two uint1024. The result is a uint1024.
+     * @param a0 A uint256 representing the lower bits of the first addend
+     * @param a1 A uint256 representing the high bits of the first addend
+     * @param a2 A uint256 representing the higher bits of the first addend
+     * @param a3 A uint256 representing the highest bits of the first addend
+     * @param b0 A uint256 representing the lower bits of the seccond addend
+     * @param b1 A uint256 representing the high bits of the seccond addend
+     * @param b2 A uint256 representing the higher bits of the seccond addend
+     * @param b3 A uint256 representing the highest bits of the seccond addend
+     * @return r0 The lower bits of the result
+     * @return r1 The high bits of the result
+     * @return r2 The higher bits of the result
+     * @return r3 The highest bits of the result
+     */
+    function add1024x1024(uint256 a0, uint256 a1, uint256 a2, uint256 a3, uint256 b0, uint256 b1, uint256 b2, uint256 b3) internal pure returns (uint256 r0, uint256 r1, uint256 r2, uint256 r3) {
+        assembly {
+            r0 := add(a0, b0)
+            r1 := add(add(a1, b1), lt(r0, b0))
+            r2 := add(add(a2, b2), lt(r1, b1))
+            r3 := add(add(a3, b3), lt(r2, b2))
+            if lt(r3, b3){
+                let ptr := mload(0x40) // Get free memory pointer
+                mstore(ptr, 0x08c379a000000000000000000000000000000000000000000000000000000000) // Selector for method Error(string)
+                mstore(add(ptr, 0x04), 0x20) // String offset
+                mstore(add(ptr, 0x24), 16) // Revert reason length
+                mstore(add(ptr, 0x44), "add1024 overflow")
+                revert(ptr, 0x64) // Revert data length is 4 bytes for selector and 3 slots of 0x20 bytes
+            }
+        }
+        /*assembly {
+            r0 := add(a0, b0)
+            let carryoverA := lt(r0, b0)
+            r1 := add(a1, b1)
+            let carryoverB := lt(r1, b1)
+            r1 := add(r1, carryoverA)
+            r2 := add(a2, b2)
+            carryoverA := lt(r2, b2)
+            r2 := add(r2, carryoverB)
+            carryoverB := lt(r3, b3)
+            r3 := add(a3, b3)
+            carryoverA := lt(r3, b3)
+            r3 := add(r3, carryoverB)
+            if carryoverA {
+                let ptr := mload(0x40) // Get free memory pointer
+                mstore(ptr, 0x08c379a000000000000000000000000000000000000000000000000000000000) // Selector for method Error(string)
+                mstore(add(ptr, 0x04), 0x20) // String offset
+                mstore(add(ptr, 0x24), 16) // Revert reason length
+                mstore(add(ptr, 0x44), "add1024 overflow")
+                revert(ptr, 0x64) // Revert data length is 4 bytes for selector and 3 slots of 0x20 bytes
+            }
+        }*/
+    }
+
+    /**
+     * @notice Calculates the difference of two uint1024. The result is a uint1024.
+     * @param a0 A uint256 representing the lower bits of the minuend
+     * @param a1 A uint256 representing the high bits of the minuend
+     * @param a2 A uint256 representing the higher bits of the minuend
+     * @param a3 A uint256 representing the highest bits of the minuend
+     * @param b0 A uint256 representing the lower bits of the subtrahend
+     * @param b1 A uint256 representing the high bits of the subtrahend
+     * @param b2 A uint256 representing the higher bits of the subtrahend
+     * @param b3 A uint256 representing the highest bits of the subtrahend
+     * @return r0 The lower bits of the result
+     * @return r1 The high bits of the result
+     * @return r2 The higher bits of the result
+     * @return r3 The highest bits of the result
+     */
+    function sub1024x1024(uint a0, uint a1, uint a2, uint256 a3, uint b0, uint b1, uint b2, uint256 b3) internal pure returns (uint r0, uint r1, uint r2, uint256 r3) {
+        if (lt1024(a0, a1, a2, a3, b0, b1, b2, b3)) revert("Uint1024: negative result sub1024x1024");
+        assembly {
+            r0 := sub(a0, b0)
+            r1 := sub(sub(a1, b1), lt(a0, b0))
+            r2 := sub(sub(a2, b2), lt(a1, b1))
+            r3 := sub(sub(a3, b3), lt(a2, b2))
+        }
+        /*assembly {
+            if or(lt(b0, a0), eq(b0, a0)) {
+                r0 := sub(a0, b0)
+            }
+            if gt(b0, a0) {
+                r0 := add(a0, sub(0, b0))
+                if iszero(a1) {
+                    a2 := sub(a2, 1)
+                }
+                a1 := sub(a1, 1)
+            }
+            let condition := or(lt(b1, a1), eq(b1, a1))
+            switch condition
+            case 0 {
+                r1 := add(a1, sub(0, b1))
+                a2 := sub(a2, 1)
+            }
+            case 1 {
+                r1 := sub(a1, b1)
+            }
+            condition := or(lt(b2, a2), eq(b2, a2))
+            switch condition
+            case 0 {
+                r2 := add(a2, sub(0, b2))
+                a3 := sub(a3, 1)
+            }
+            case 1 {
+                r2 := sub(a2, b2)
+            }
+            r3 := sub(a3, b3)
+        }*/
     }
 }
